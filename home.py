a435e9c5d21787e548843db7dbd10b00fc56bef0
LucasGCRamos <117207058+LucasGCRamos@users.noreply.github.com>
2024-12-11 21:30:15 -0300

    criada análise exploratória no streamlit

--- home.py
+++ home.py
@@ -1,14 +1,26 @@
-import pandas as pd
 import streamlit as st
-import plotly.express as px
 
-st.write('Predict Students Dropout and Academic Success')
+# Configurações da página
+st.set_page_config(
+    page_title="ProAluno",
+    layout="wide",
+)
 
-def load_data():
-    data = pd.read_csv('pisi3_database/predict_dropout.csv', delimiter=';')
-    return data
+st.write("""
+    <h2>Sobre o Projeto</h2>
+    <p>Nesta análise foi utilizado o dataset disponível no Kaggle: <a href='https://www.kaggle.com/datasets/syedfaizanalii/predict-students-dropout-and-academic-success'>Predict Students Dropout and Academic Success</a>.</p>
+""", unsafe_allow_html=True)
 
-st.subheader('Raw data:')
-dataset=load_data()
-st.write(dataset)
+
+st.divider()
+st.write("""
+        <h2>Perguntas que orientam a análise</h2>
+        <p>Para guiar a análise dos dados, foram definidas as seguintes perguntas:</p>
+        <h3>Pergunta 1</h3>
+        <p>De que forma fatores socioeconômicos, como a ocupação dos pais, a regularidade do pagamento das mensalidades, a condição de endividamento e a concessão de bolsa de estudos, podem ser utilizados para prever a probabilidade de evasão de alunos universitários?</p>
+        <h3>Pergunta 2</h3>
+        <p>Ao agrupar alunos com base em características acadêmicas, socioeconômicas e de adaptação, quais padrões emergem na formação de grupos e como esses padrões podem ser utilizados para categorizar alunos em diferentes níveis de risco acadêmico?
+</p>
+""", unsafe_allow_html=True)
+
 
